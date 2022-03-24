2a8489bd4f278ab3fec754c078313bbecb94ca99
mobval <mobval@qq.com>
2022-03-24 12:43:12 +0800

    pwn_1

--- hello.c
+++ hello.c
@@ -1,5 +1,14 @@
+/*
+ * @Author: MOBval 
+ * @Date: 2022-02-09 21:08:08 
+ * @Last Modified by: MOBval
+ * @Last Modified time: 2022-02-09 21:57:22
+ */
 #include<stdio.h>
+#include<signal.h>
 int main(){
-    printf("hello world");
-    
+    float ((f));
+    ((f))=1.0;
+    printf("%f",((f)));
+
 }
